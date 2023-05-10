c4ac8df5f6e5b70ef67c7a663fbff270e86ef8ce
Ida Mäkelä <idamakela98@gmail.com>
2023-05-10 11:22:27 +0200

    first get req and nodemon

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
-const movies = require('./routes/movies')
+const movies = require('./routes/movies');
 
 const app = express();
 const port = 3001;
 
+//MIDDLEWARE
+// app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello world')
-})
+  res.json('Hello World!');
+});
+
+app.use('/movies', movies);
 
 app.listen(port, () => {
-    console.log(
-      `App is listening on port: ${port}. Visit localhost: http://localhost:${port}`
-    );
-})
+  console.log(
+    `App is listening on port: ${port}. Visit localhost: http://localhost:${port}`
+  );
+});
--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const mockData = require('../mockData');
 
-let movies = mockData; 
+let movies = mockData;
+
+//GET all movies
+router.get('/', (req, res) => {
+  res.json(movies);
+});
 
 module.exports = router;
